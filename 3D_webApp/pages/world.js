0fea679231fd9114be1dd4d421e9242b79cadbcb
Adarsh Jena <76236137+adarshnjena@users.noreply.github.com>
2023-02-25 08:08:39 +0530

    folder added for 2dApp

--- 3D_webApp/pages/world.js
+++ 3D_webApp/pages/world.js
@@ -4,11 +4,15 @@ import Experience from "../components/Experience";
 const world = () => {
   return (
     <>
-      <Canvas camera={{ position: [-70, 20, 30], fov: 45 }} rotation={[0,0,2]} shadows>
+      <Canvas
+        camera={{ position: [-70, 20, 30], fov: 45 }}
+        rotation={[0, 0, 2]}
+        shadows
+      >
         <Experience />
       </Canvas>
     </>
-  )
-}
+  );
+};
 
-export default world
+export default world;
